1858c191fa7ee65b8e63ec0bf842464c10307349
jun980401 <ldj980401@naver.com>
2022-06-05 19:05:20 +0900

    Update ssh_keys.cc

--- selfdrive/ui/qt/widgets/ssh_keys.cc
+++ selfdrive/ui/qt/widgets/ssh_keys.cc
@@ -4,14 +4,14 @@
 #include "selfdrive/ui/qt/api.h"
 #include "selfdrive/ui/qt/widgets/input.h"
 
-SshControl::SshControl() : ButtonControl("SSH 키", "", "경고: 이 경우 GitHub 설정의 모든 공개 키에 SSH 액세스 권한이 부여됩니다. 자신의 GitHub 사용자명이 아닌 다른 사용자명을 입력하지 마십시오. comma 직원은 GitHub 사용자명 추가를 절대로 요구하지 않습니다.") {
+SshControl::SshControl() : ButtonControl("SSH 키", "", "경고: 이 경우 GitHub 설정의 모든 공개 키에 SSH 액세스 권한이 부여됩니다. 자신의 GitHub 사용자 이름이 아닌 다른 사용자 이름을 입력하지 마십시오. comma 직원은 GitHub 사용자 이름 추가를 절대로 요구하지 않습니다.") {
   username_label.setAlignment(Qt::AlignRight | Qt::AlignVCenter);
   username_label.setStyleSheet("color: #aaaaaa");
   hlayout->insertWidget(1, &username_label);
 
   QObject::connect(this, &ButtonControl::clicked, [=]() {
     if (text() == "ADD") {
-      QString username = InputDialog::getText("GitHub 사용자명을 입력하세요.", this);
+      QString username = InputDialog::getText("GitHub 사용자 이름을 입력하세요.", this);
       if (username.length() > 0) {
         setText("LOADING");
         setEnabled(false);
